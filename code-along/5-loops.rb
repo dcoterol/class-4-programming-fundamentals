08a609f8fe4f8cc3e8ed1c1752de3bad7b7026f3
Dany Cotero <dany.cotero@kellogg.northwestern.edu>
2023-05-30 15:11:47 +0000

    loops code-along and lab

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -8,4 +8,31 @@
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+# index = 0
+# loop do
+#     if index == 5
+#         break
+#     end
+#     puts "tacos!"
+#     index = index + 1
+# end
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+
+index = 0
+loop do
+    if index == tacos.size # I want to see how many items on my array so I ask
+        break
+    end
+
+    taco = tacos[index]
+
+    puts "#{taco} tacos!"
+    index = index + 1
+end
+
+#Shorthand for structure above    
+    for taco in tacos           # for ______ in ___(array tou are dealing with)_____
+        puts "#{taco} tacos!"   #       # do something
+    end                         # end
