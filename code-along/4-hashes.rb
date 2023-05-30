7d15726ea661a1a9306277415fba1f90c85c3154
Dany Cotero <dany.cotero@kellogg.northwestern.edu>
2023-05-30 14:22:55 +0000

    HASHES class and lab

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,29 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    "name" => "Dany", 
+    "location" => { "city" => "Evanston", "state" => "IL" }, 
+        # the values within a hash can be anything, even other hashes or arrays 
+        # "location" => { "city" => "Evanston", "state" => "IL" },
+        # VS "location" => Evanston, IL
+    "timeline" => [
+        {"status" => "Studying sofdev", "posted_at" => "8:00pm"},
+        {"status" => "Still learning", "posted_at" => "8:30pm"},
+        {"status" => "Want to sleep", "posted_at" => "9:23pm"}
+        ]
+}    
+puts profile    
 
 # Accessing data from the hash
+puts profile ["name"]
+puts profile ["status"] # this one won't work cause we changed the status to timeline
+puts profile ["location"]["city"]
 
 # More Complex Hashes
+# profile ["status"] = "Still learning"
+    # Usually status is changing so we can create a timeline 
+# profile ["phone number"] = "773-766-5651"
+# puts profile
+
+puts profile ["timeline"][2]["status"]
